1e5cd69d3da121e25fbf44d50843d918573ce817
Michael Coxon <michaelccoxon@gmail.com>
2025-11-19 21:44:15 +0800

    migrate footer to common

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Header from "./components/Header";
+import Footer from "@/components/Footer";
 
 export default function GeneralLayout({
   children,
@@ -26,6 +27,7 @@ export default function GeneralLayout({
           </article>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection";
 import ServicesSection from "./components/ServicesSection";
 import AboutSection from "./components/AboutSection";
 import ContactSection from "./components/ContactSection";
-import Footer from "./components/Footer";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
