3c7678d608fe8e40a13a01effc57750c699c176e
Michael Coxon <michaelccoxon@gmail.com>
2025-11-26 20:37:40 +0800

    fix broken assets

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
